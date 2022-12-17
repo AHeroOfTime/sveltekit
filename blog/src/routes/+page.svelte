e3b5f799ad5ae72758b5114307e7853139c0dcfa
Aaron Rodriguez <aaroner@yahoo.com>
2022-12-17 17:46:29 -0600

    9/31

--- blog/src/routes/+page.svelte
+++ blog/src/routes/+page.svelte
@@ -1,11 +1,25 @@
 <script>
   import Test from '$lib/Test.svelte';
   import '$db/start';
+  export let data;
+
+  $: ({ latest_episode, all_episodes } = data)
+  // const { latest_episode } = data;
+  // $: makes ^^ reactive, so you get fresh data when the data is updated
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h3>{latest_episode.title}</h3>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<a href="/about">About Us</a>
+<!-- <a href="/about">About Us</a> -->
+<nav>
+  <ul>
+    {#each all_episodes as episode}
+      <li>
+        <a href="/episodes/{episode.slug}">{episode.title}</a>
+      </li>    
+    {/each}
+  </ul>
+</nav>
 
 <Test />
