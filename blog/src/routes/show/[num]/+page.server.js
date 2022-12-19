9af075907df7a22c3983472dff8cc24618400018
Aaron Rodriguez <aaroner@yahoo.com>
2022-12-19 12:28:59 -0600

    13/31

--- blog/src/routes/show/[num]/+page.server.js
+++ blog/src/routes/show/[num]/+page.server.js
@@ -1,8 +1,13 @@
-export async function load({ fetch, params, parent }) {
-	const parent_data = await parent();
+export async function load({ fetch, params, parent, setHeaders }) {
+	// const parent_data = await parent();
 	// console.log(parent_data);
 	const res = await fetch(`https://syntax.fm/api/shows/${params.num}`);
 	const data = await res.json();
+
+	setHeaders({
+		'Cache-Control': 'max-age=3600'
+	});
+
 	return {
 		episode: data
 	};
